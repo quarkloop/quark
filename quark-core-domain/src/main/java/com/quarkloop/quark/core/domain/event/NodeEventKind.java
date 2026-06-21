3c0831a269bbe32315dff6079d257dba9de3a705
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2026-06-21 19:09:43 +0000

    feat(domain): add NodeEventKind enum

---
+++ quark-core-domain/src/main/java/com/quarkloop/quark/core/domain/event/NodeEventKind.java
@@ -0,0 +1,19 @@
+package com.quarkloop.quark.core.domain.event;
+
+/**
+ * All possible event kinds defined in the spec.
+ */
+public enum NodeEventKind {
+    NODE_CREATED,
+    NODE_UPDATED,
+    NODE_STATE_CHANGED,
+    NODE_DATA_RECEIVED,
+    NODE_DATA_PRODUCED,
+    NODE_EXECUTION_STARTED,
+    NODE_EXECUTION_COMPLETED,
+    NODE_EXECUTION_FAILED,
+    NODE_QUERY_RECEIVED,
+    NODE_QUERY_RESPONDED,
+    NODE_POLICY_EVALUATED,
+    NODE_POLICY_VIOLATED
+}
